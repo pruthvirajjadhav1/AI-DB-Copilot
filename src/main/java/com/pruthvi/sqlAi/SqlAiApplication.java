8ffa6846840bb28a545af1d9c21d222c2c608015
Pruthviraj Jadhav <pruthvirajjadhav@Pruthviraj-ka-MacBook-Pro.local>
2025-11-06 00:33:51 +0530

    initial

---
+++ src/main/java/com/pruthvi/sqlAi/SqlAiApplication.java
@@ -0,0 +1,13 @@
+package com.pruthvi.sqlAi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pruthvi/sqlAi/SqlAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pruthvi.sqlAi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
